79200cbbc65260fad74522f9010463c5e27c54ff
MalikTalha03 <mtnaqshbandi2003@gmail.com>
2024-01-01 01:05:52 +0500

    Add supptransaction model

---
+++ models/supptransaction.js
@@ -0,0 +1,29 @@
+const moongoose = require('mongoose');
+const SupplierOrder = require('./supplierorder');
+
+const suppTransactionSchema = new moongoose.Schema({
+    transactionID: {
+        type: Number,
+        required: true
+    },
+    transactionType: {
+        type: String,
+        required: true
+    },
+    transactionDate: {
+        type: Date,
+        required: true
+    },
+    totalAmount: {
+        type: Number,
+        required: true
+    },
+    orderID: {
+        type: moongoose.Schema.Types.ObjectId,
+        ref: 'SupplierOrder',
+        required: true
+    },
+});
+
+const SuppTransaction = moongoose.model('SuppTransaction', suppTransactionSchema);
+module.exports = SuppTransaction;
