ab1ac15a7710943f4c8c6c7b0b41e3e5fe598b9c
MalikTalha03 <mtnaqshbandi2003@gmail.com>
2024-01-02 15:53:26 +0500

    Add category routes

---
+++ routes/category.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const router = express.Router();
+const CategoryModel = require('../models/category');
+
+router.get('/', async (req, res) => {
+    try {
+        const categories = await CategoryModel.find();
+        res.send(categories);
+    }
+    catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}
+);
+
+router.get('/:id', async(req, res) => {
+    try {
+        const category = await CategoryModel.findById(req.params.id);
+        res.send(category);
+    }
+    catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}
+);
+
+router.post('/', async (req, res) => {
+    const category = new CategoryModel({
+        id: req.body.id,
+        name: req.body.name,
+    });
+    try {
+        const newCategory = await category.save();
+        res.status(201).json(newCategory);
+    }
+    catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+}
+);
+
+router.patch('/:id', async (req, res) => {
+    try {
+        const category = await CategoryModel.findById(req.params.id);
+        if (req.body.name) {
+            category.name = req.body.name;
+        }
+        const updatedCategory = await category.save();
+        res.json(updatedCategory);
+    }
+    catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+}
+);
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const category = await CategoryModel.findById(req.params.id);
+        const deletedCategory = await category.remove();
+        res.json(deletedCategory);
+    }
+    catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}
+);
+
+module.exports = router;
