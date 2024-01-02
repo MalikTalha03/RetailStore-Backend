3c6f363f5f02579d5aec58ca43005c979af900c8
MalikTalha03 <mtnaqshbandi2003@gmail.com>
2024-01-02 15:43:10 +0500

    Add product routes to handle CRUD operations

---
+++ routes/products.js
@@ -0,0 +1,84 @@
+const express = require('express');
+const router = express.Router();
+const ProductModel = require('../models/products');
+
+router.get('/', async (req, res) => {
+    try {
+        const products = await ProductModel.find();
+        res.send(products);
+    }
+    catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+router.get('/:id', async(req, res) => {
+    try {
+        const product = await ProductModel.findById(req.params.id);
+        res.send(product);
+    }
+    catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}
+);
+
+router.post('/', async (req, res) => {
+    const product = new ProductModel({
+        id: req.body.id,
+        name: req.body.name,
+        price: req.body.price,
+        category: req.body.category,
+        supplierID: req.body.supplierID,
+        inventory: req.body.inventory
+    });
+    try {
+        const newProduct = await product.save();
+        res.status(201).json(newProduct);
+    }
+    catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+}
+);
+
+router.patch('/:id', async (req, res) => {
+    try {
+        const product = await ProductModel.findById(req.params.id);
+        if (req.body.name) {
+            product.name = req.body.name;
+        }
+        if (req.body.price) {
+            product.price = req.body.price;
+        }
+        if (req.body.category) {
+            product.category = req.body.category;
+        }
+        if (req.body.supplierID) {
+            product.supplierID = req.body.supplierID;
+        }
+        if (req.body.inventory) {
+            product.inventory = req.body.inventory;
+        }
+        await product.save();
+        res.send(product);
+    }
+    catch (err) {
+        res.status(404).json({ message: err.message });
+    }
+}
+);
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const product = await ProductModel.findById(req.params.id);
+        await product.remove();
+        res.json({ message: 'Deleted Product' });
+    }
+    catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}
+);
+
+module.exports = router;
