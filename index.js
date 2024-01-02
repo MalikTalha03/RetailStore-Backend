678ed1439db1ff35ef58304bc5a538c9d46800f3
MalikTalha03 <mtnaqshbandi2003@gmail.com>
2024-01-02 15:51:47 +0500

    Add CORS, product router, and MongoDB connection

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const productRouter = require('./routes/products');
+require('dotenv').config();
+const connectionurl = process.env.MONGO_URL;
+const bodyParser = require('body-parser');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+app.use('/products', productRouter);
+app.use(bodyParser.urlencoded({ extended: false }));
+
+mongoose.connect(connectionurl, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+app.listen(3000, () => {
+    console.log('Server started at port 3000');
+}
+);
