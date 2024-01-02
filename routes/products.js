b0debf5d3ed8998edbfccaff60df22dd1612ba80
MalikTalha03 <mtnaqshbandi2003@gmail.com>
2024-01-02 17:04:26 +0500

    Update product model import

--- routes/products.js
+++ routes/products.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const ProductModel = require('../models/products');
+const ProductModel = require('../models/product');
 
 router.get('/', async (req, res) => {
     try {
