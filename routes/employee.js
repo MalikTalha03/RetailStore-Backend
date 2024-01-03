3a190079ed2db768f8e44ca97e083eef49cf60eb
MalikTalha03 <mtnaqshbandi2003@gmail.com>
2024-01-03 21:13:16 +0500

    Add employee routes and CRUD operations

---
+++ routes/employee.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const router = express.Router();
+const EmployeeModel = require('../models/employee');
+
+router.get('/', async (req, res) => {
+    try {
+        const employees = await EmployeeModel.find();
+        res.send(employees);
+    }
+    catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}
+);
+
+router.get('/:id', async(req, res) => {
+    try {
+        const employee = await EmployeeModel.findById(req.params.id);
+        res.send(employee);
+    }
+    catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}
+);
+
+router.post('/', async (req, res) => {
+    const employee = new EmployeeModel({
+        id : req.body.id,
+        firstname : req.body.firstname,
+        lastname : req.body.lastname,
+        contact : req.body.contact,
+        address : req.body.address,
+        salary : req.body.salary,
+        position : req.body.position,
+        password : req.body.password
+    });
+    try {
+        const newEmployee = await employee.save();
+        res.status(201).json(newEmployee);
+    }
+    catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+}
+);
+
+router.patch('/:id', async (req, res) => {
+    try {
+        const employee = await EmployeeModel.findById(req.params.id);
+        if (req.body.firstname) {
+            employee.firstname = req.body.firstname;
+        }
+        if (req.body.lastname) {
+            employee.lastname = req.body.lastname;
+        }
+        if (req.body.contact) {
+            employee.contact = req.body.contact;
+        }
+        if (req.body.address) {
+            employee.address = req.body.address;
+        }
+        if (req.body.salary) {
+            employee.salary = req.body.salary;
+        }
+        if (req.body.position) {
+            employee.position = req.body.position;
+        }
+        if (req.body.password) {
+            employee.password = req.body.password;
+        }
+        const updatedEmployee = await employee.save();
+        res.json(updatedEmployee);
+    }
+    catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+}
+);
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const employee = await EmployeeModel.findById(req.params.id);
+        const deletedEmployee = await employee.remove();
+        res.json(deletedEmployee);
+    }
+    catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}
+);
+
+module.exports = router;
