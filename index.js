acc49f9f6dd97031cce41985db93ff6e590a1da9
MalikTalha03 <mtnaqshbandi2003@gmail.com>
2024-01-03 23:04:53 +0500

    Add new routes for credit customers, suppliers, orders, order details, transactions, and employees

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const productRouter = require('./routes/products');
 require('dotenv').config();
 const connectionurl = process.env.MONGO_URL;
 const bodyParser = require('body-parser');
+
 const categoryRouter = require('./routes/category');
 const customerRouter = require('./routes/customer');
+const creditCustomerRouter = require('./routes/creditcustomer');
+const supplierRouter = require('./routes/supplier');
+const supplierOrderRouter = require('./routes/supplierorder');
+const supplierOrderDetailRouter = require('./routes/supporderdetail');
+const supplierTransactionRouter = require('./routes/supptransactions');
+const customerOrderRouter = require('./routes/customerorder');
+const customerOrderDetailRouter = require('./routes/custorderdetail');
+const customerTransactionRouter = require('./routes/custtransactions');
+const employeeRouter = require('./routes/employee');
+const productRouter = require('./routes/products');
+
 
 const app = express();
 app.use(express.json());
@@ -15,6 +26,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/products', productRouter);
 app.use('/category', categoryRouter);
 app.use('/customer', customerRouter);
+app.use('/creditcustomer', creditCustomerRouter);
+app.use('/supplier', supplierRouter);
+app.use('/supplierorder', supplierOrderRouter);
+app.use('/supplierorderdetail', supplierOrderDetailRouter);
+app.use('/suppliertransaction', supplierTransactionRouter);
+app.use('/customerorder', customerOrderRouter);
+app.use('/customerorderdetail', customerOrderDetailRouter);
+app.use('/customertransaction', customerTransactionRouter);
+app.use('/employee', employeeRouter);
 
 
 mongoose.connect(connectionurl, { useNewUrlParser: true, useUnifiedTopology: true })
