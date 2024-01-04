b12768e762a8cbae701e87d9c4efd5a24a66ecb4
MalikTalha03 <mtnaqshbandi2003@gmail.com>
2024-01-04 11:02:24 +0500

    Add auto-incrementing ID to employee model

--- models/employee.js
+++ models/employee.js
@@ -35,5 +35,18 @@ const employeeSchema = new moongoose.Schema({
     },
 });
 
+employeeSchema.pre('save', function(next) {
+    if(!this.id){
+        const maxid = this.constructor.find().sort({id: -1}).limit(1).then(result => {
+            this.id = result[0].id + 1;
+            next();
+        });
+    }
+    else{
+        next();
+    }
+}
+);
+
 const Employee = moongoose.model('Employee', employeeSchema);
 module.exports = Employee;
