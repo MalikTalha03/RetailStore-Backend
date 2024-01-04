4929bf23ad5eada6c50f73ab2e7553e59c737abf
MalikTalha03 <mtnaqshbandi2003@gmail.com>
2024-01-04 11:02:29 +0500

    Add auto-incrementing ID to product model

--- models/product.js
+++ models/product.js
@@ -26,5 +26,18 @@ const productSchema = new mongoose.Schema({
     }
 });
 
+productSchema.pre('save', function(next) {
+    if(!this.id){
+        const maxid = this.constructor.find().sort({id: -1}).limit(1).then(result => {
+            this.id = result[0].id + 1;
+            next();
+        });
+    }
+    else{
+        next();
+    }
+}
+);
+
 const Product = mongoose.model('Product', productSchema);
 module.exports = Product;
