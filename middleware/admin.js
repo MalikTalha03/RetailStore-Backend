321ce58e8e011af0f68405ad4da8ba3600bb667b
MalikTalha03 <mtnaqshbandi2003@gmail.com>
2024-01-06 17:45:07 +0500

    Add admin middleware for authorization

---
+++ middleware/admin.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const secret = process.env.HastString;
+
+const isAdmin = (req, res, next) => {
+    if (req.headers.authorization == null) {
+        return res.status(401).json({ message: 'Admin Token is required to access' });
+    }
+    const token = req.headers.authorization.split(' ')[1];
+    if (token == null) {
+        return res.status(401).json({ message: 'Token Not Found' });
+    }
+    const decoded = jwt.verify(token, secret);
+    if (decoded.position == 'Admin') {
+        next();
+    } else {
+        res.status(401).json({ message: 'Unauthorized' });
+    }
+}
+
+module.exports = { isAdmin };
