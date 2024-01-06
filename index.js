a4109340dd4cdf431fc53f022cc5793443bc3384
MalikTalha03 <mtnaqshbandi2003@gmail.com>
2024-01-06 14:51:50 +0500

    Refactor CORS configuration and update server port

--- index.js
+++ index.js
@@ -25,12 +25,7 @@ const authRouter = require('./routes/auth');
 
 const app = express();
 app.use(express.json());
-app.use(cors(
-    {
-        origin: 'http://localhost:3000',
-        credentials: true
-    }
-));
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
@@ -69,7 +64,7 @@ mongoose.connect(connectionurl, { useNewUrlParser: true, useUnifiedTopology: tru
         console.log(err);
     });
 
-app.listen(3001, () => {
-    console.log('Server started at port 3001');
+app.listen(3000, () => {
+    console.log('Server started at port 3000');
 }
 );
