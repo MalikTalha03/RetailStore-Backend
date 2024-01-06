ce0a0206c6c9bee3fc283f9ffeafff83c9432413
MalikTalha03 <mtnaqshbandi2003@gmail.com>
2024-01-06 17:45:52 +0500

    Add register route to handle user registration

---
+++ routes/register.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const { register } = require('../middleware/auth');
+
+router.post('/', register, (req, res) => {
+    res.status(201).json({ message: 'Register Successful' });
+}
+);
+
+module.exports = router;
