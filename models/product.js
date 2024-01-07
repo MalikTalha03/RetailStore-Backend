abc66e2c194cdcc3133434e5ab49794560a5fb35
MalikTalha03 <mtnaqshbandi2003@gmail.com>
2024-01-08 00:05:51 +0500

    Update product schema to include category reference

--- models/product.js
+++ models/product.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const categorySchema = require('./category');
 const productSchema = new mongoose.Schema({
     id: {
         type: Number,
@@ -13,8 +14,9 @@ const productSchema = new mongoose.Schema({
         required: [true, 'Please enter product price']
     },
     category: {
-        type: String,
-        required: [true, 'Please enter product category']
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category',
+        required: true,
     },
     supplierID: {
         type: Number,
@@ -26,28 +28,6 @@ const productSchema = new mongoose.Schema({
     }
 });
 
-productSchema.pre('save', function(next) {
-    if(!this.id){
-        try {
-            const result = this.constructor.find().sort({ id: -1 }).limit(1);
-            if(result.length > 0){
-                this.id = result[0].id + 1;
-            }
-            else{
-                this.id = 1;
-            }
-            next();
-        } 
-        catch (error) {
-            console.error(error);
-            next(error);
-        }
-    }
-    else{
-        next();
-    }
-}
-);
 
 const Product = mongoose.model('Product', productSchema);
 module.exports = Product;
