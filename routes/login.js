0d184ac8fb39c7f362364142889150954940ea03
MalikTalha03 <mtnaqshbandi2003@gmail.com>
2024-01-16 21:36:55 +0500

    Add loggedIn middleware to login route for token verification

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { login } = require('../middleware/auth');
+const { login, loggedIn } = require('../middleware/auth');
 
 router.post ('/', login, (req, res) => {
     res.status(200).json({ message: 'Login Successful' });
+}
+);
+
+router.get('/',loggedIn, (req, res) => {
+    res.status(200).json({ message: 'JWT OK' });
 }
 );
 
