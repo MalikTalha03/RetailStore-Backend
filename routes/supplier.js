c56b34c89fe8e78cf3aa68a1322db272654268fa
MalikTalha03 <mtnaqshbandi2003@gmail.com>
2024-01-17 23:04:40 +0500

    Update product price in supplier order details

--- routes/supplier.js
+++ routes/supplier.js
@@ -76,7 +76,7 @@ router.patch('/:id/orders/:orderId/details', async (req, res) => {
         if (!order) {
             return res.status(404).json({ message: 'Order not found' });
         }
-        const product = await ProductModel.findByIdAndUpdate(req.body.productid, { $inc: { inventory: req.body.qty, price: req.body.unitPrice } });
+        const product = await ProductModel.findByIdAndUpdate(req.body.productid, { $inc: { inventory: req.body.qty, price: req.body.saleprice } });
         if (!product) {
             return res.status(404).json({ message: 'Product not found' });
         }
